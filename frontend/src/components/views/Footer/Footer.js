7b06127ef9b9b4e6adfaa77f453da81395596f6f
mikolajchm <chmielewski.5454@gmail.com>
2025-06-03 16:54:59 +0200

    feat: update frontend create ((NavBar, Footer)-features,Home page - structure

---
+++ frontend/src/components/views/Footer/Footer.js
@@ -0,0 +1,11 @@
+import styles from ('./Footer.module.scss');
+
+const Footer = () => {
+  return (
+    <div> 
+      <h1>Hospitall App</h1>
+    </div>
+  )
+}
+
+export default Footer;
---
+++ frontend/src/components/views/NavBar/NavBar.js
@@ -0,0 +1,11 @@
+import styles from ('./NavBar.module.scss');
+
+const NavBar = () => {
+  return (
+    <div> 
+      <h1> NavBar</h1>
+    </div>
+  )
+}
+
+export default NavBar;
