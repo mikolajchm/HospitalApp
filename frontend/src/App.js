1c497ae544a956e7011b57550c879d8e3c3b7c6a
mikolajchm <chmielewski.5454@gmail.com>
2025-06-08 14:37:09 +0200

    fix: fix bugs in frontend files, update components

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,8 +1,26 @@
+import { Routes, Route } from 'react-router-dom';
+import { Navigate } from 'react-router-dom';
+import Container from 'react-bootstrap/Container';
+import NavBar from './components/views/NavBar/NavBar';
+import Footer from './components/views/Footer/Footer';
+import NotFound from './components/pages/NotFound/NotFound';
+import Login from './components/pages/Login/Login';
+
+
 const App = () => {
-  reutrn (
-    <div>
-      <h1>App</h1>
-    </div>
+
+  return (
+    <main>
+      <NavBar />
+      <Container>
+        <Routes>
+          <Route path="/" element={<Navigate to="/login" replace />} />
+          <Route path='/login' element={<Login />} />
+          <Route path="*" element={<NotFound />} />
+        </Routes>
+      </Container>
+      <Footer />
+    </main>
   )
 };
 
--- frontend/src/components/views/NavBar/NavBar.js
+++ frontend/src/components/views/NavBar/NavBar.js
@@ -1,4 +1,4 @@
-import styles from ('./NavBar.module.scss');
+import styles from './NavBar.module.scss';
 
 const NavBar = () => {
   return (
