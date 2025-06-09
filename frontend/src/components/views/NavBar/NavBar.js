ef19bbc152eecc9d8ee1dc8558829ab12e243c97
mikolajchm <chmielewski.5454@gmail.com>
2025-06-09 14:25:58 +0200

    feat: add NavBar-frontend

--- frontend/src/components/views/NavBar/NavBar.js
+++ frontend/src/components/views/NavBar/NavBar.js
@@ -1,11 +1,50 @@
+import React from 'react';
+import { Navbar, Container, Nav } from 'react-bootstrap';
+import { Link } from 'react-router-dom';
 import styles from './NavBar.module.scss';
+import { getUser } from '../../../redux/userRedux';
+import { useSelector } from 'react-redux';
 
-const NavBar = () => {
+function NavBar({ user }) {
+
+  const user = useSelector(getUser);
+
   return (
-    <div> 
-      <h1> NavBar</h1>
-    </div>
-  )
+    <Navbar bg="light" expand="lg" className={styles.navbar}>
+      <Container>
+        <Navbar.Brand as={Link} to="/" className={styles.brand}>
+          HospitallApp
+        </Navbar.Brand>
+        <Nav className="ms-auto">
+          <Nav.Link as={Link} to="/" className={styles.navLink}>
+            Home
+          </Nav.Link>
+          {user ? (
+            <>
+              <Nav.Link as={Link} to="/ad/add/ad" className={styles.navLink}>
+                Add patient
+              </Nav.Link>
+              <Nav.Link as={Link} to="/ad/add/ad" className={styles.navLink}>
+                Add attribution
+              </Nav.Link>
+              <Nav.Link as={Link} to="/logout" className={styles.navLink}>
+                LogOut
+              </Nav.Link>
+            </>
+          ) : (
+            <>
+              <Nav.Link as={Link} to="/login" className={styles.navLink}>
+                LogIn
+              </Nav.Link>
+              <Nav.Link as={Link} to="/register" className={styles.navLink}>
+                Sign up
+              </Nav.Link>
+            </>
+          )}
+        </Nav>
+      </Container>
+    </Navbar>
+  );
 }
 
 export default NavBar;
